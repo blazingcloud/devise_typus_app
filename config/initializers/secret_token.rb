cfba73f967be81e9d69577bca4b44892df00b497
Sarah Allen <sarah@ultrasaurus.com>
2011-02-14 21:22:24 -0800

    blank app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TypusTestApp::Application.config.secret_token = '3c50373bc3884393b6559e44e7cb021432e985f8b277793ec238102c54a0a508d10b39cd07ad4d91f957eb953eb45c65e79553f2d9abce41448417a9695cfcc2'
