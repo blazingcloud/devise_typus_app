861de2fc2c5172b0e5f0271c5dc9e8837927c948
Sarah Allen <sarah@ultrasaurus.com>
2011-02-14 21:43:49 -0800

    scaffold for testing

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 TypusTestApp::Application.routes.draw do
+  root :to => "messages#index"
+  resources :messages
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
---
+++ spec/views/messages/index.html.erb_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+
+describe "messages/index.html.erb" do
+  before(:each) do
+    assign(:messages, [
+      stub_model(Message,
+        :title => "Title",
+        :content => "MyText"
+      ),
+      stub_model(Message,
+        :title => "Title",
+        :content => "MyText"
+      )
+    ])
+  end
+
+  it "renders a list of messages" do
+    render
+    # Run the generator again with the --webrat flag if you want to use webrat matchers
+    assert_select "tr>td", :text => "Title".to_s, :count => 2
+    # Run the generator again with the --webrat flag if you want to use webrat matchers
+    assert_select "tr>td", :text => "MyText".to_s, :count => 2
+  end
+end
